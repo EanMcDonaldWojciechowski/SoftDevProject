f54673b22196ef2e994fd08ac585c9aa439db6fe
Hiren Patel <hirenapatel1109@gmail.com>
2020-03-18 14:29:07 -0500

    testing file name

--- test/sorToDataframe.cpp
+++ test/sorToDataframe.cpp
@@ -2,10 +2,19 @@
 #include "../src/application/sorer.h"
 
 int main(int argc, char** argv) {
-  char* file_name = "../../data/sampleData.sor";
-  FILE *f = fopen(file_name, "r");
+  // char* file_name = "../../data/sampleData.sor";
+  char* file_name = "sampleData.sor";
+  FILE *f = fopen("./sampleData.sor", "r");
+  if (f == NULL) {
+    std::cout << "ERROR opening file." << "\n";
+  } else {
+    std::cout << "IT WORRRKEDDDDDDDDDDDDDDDDD" << "\n";
+  }
   SOR* reader = new SOR();
-  reader->read(f, 0, 100000);
+  std::cout << "created sor" << "\n";
+  reader->read(f, 0, 10000);
+  std::cout << "reading file" << "\n";
   DataFrame *df = reader->sorToDataframe();
+  std::cout << "past create df" << "\n";
   std::cout << "cols size " << df->ncols();
 }
